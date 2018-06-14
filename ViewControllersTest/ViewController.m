1f5e26ef5c6a3c326c16a96b3ac94fc0816d1b2c
Admin <admin@mac-admin.local>
2018-06-14 21:16:16 +0300

    Initial Commit

---
+++ ViewControllersTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ViewControllersTest
+//
+//  Created by Admin on 14.06.2018.
+//  Copyright © 2018 SergioLechini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
