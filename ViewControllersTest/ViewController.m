dc0026df5c5bea396c73cc7622eea906f3591e00
Admin <admin@mac-admin.local>
2018-06-16 22:03:52 +0300

    123

--- ViewControllersTest/ViewController.m
+++ ViewControllersTest/ViewController.m
@@ -14,12 +14,106 @@
 
 @implementation ViewController
 
+#pragma mark - Loading
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"View did load");
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+        
+        self.view.backgroundColor = [UIColor greenColor];
+    
+    else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        
+        self.view.backgroundColor = [UIColor blueColor];
+        
+    }
+    
 }
 
+- (void)loadView {
+    
+    [super loadView];
+    
+    NSLog(@"Load view");
+    
+}
 
+#pragma mark - Views
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    NSLog(@"view will appear");
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    NSLog(@"view did appear");
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    
+    [super viewWillDisappear:animated];
+    NSLog(@"view will disappear");
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated; {
+    
+    [super viewDidDisappear:animated];
+    NSLog(@"view did disappear");
+    
+}
+
+- (void)viewWillLayoutSubviews {
+    
+    [super viewWillLayoutSubviews];
+    NSLog(@"view will layout subviews");
+    
+}
+
+- (void)viewDidLayoutSubviews {
+
+    [super viewDidLayoutSubviews];
+    NSLog(@"view did layout subvies");
+    
+}
+
+#pragma mark - Orientation
+
+- (BOOL)shouldAutorotate {
+
+    return true;
+
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+
+    return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft;
+    
+}
+//
+//- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+//
+//}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+
+    NSLog(@"willRotateToInterfaceOrientation : from %d to %d", self.interfaceOrientation  ,toInterfaceOrientation);
+    
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+
+    NSLog(@"didRotateFromInterfaceOrientation : from %d to %d", fromInterfaceOrientation, self.interfaceOrientation);
+    
+}
+
+#pragma mark - Memory
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
